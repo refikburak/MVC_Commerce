dc72215a4068a2a825ff0d3540f3a8658f72f6fc
Burak Akbas <refikburakakbas@gmail.com>
2022-12-06 00:42:45 +0300

    Add project files.

---
+++ MVC_Commerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
